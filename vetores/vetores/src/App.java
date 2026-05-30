3c2cd7ab1a81b5da7b08b754108b54b642b2d104
Joao Rocha <jpbrandao1104@gmail.com>
2026-05-29 21:43:16 -0300

    Usando Scanner

--- vetores/vetores/src/App.java
+++ vetores/vetores/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        String nome = leia.nextLine();
+
+        System.out.printf("O seu nome e: %s \n", nome);
+
+        leia.close();
     }
 }
